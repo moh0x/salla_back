b4d4e88ed62f1e75bad615146efabf372f82f908
moh0x <141264101+moh0x@users.noreply.github.com>
2024-07-31 02:57:20 -0700

    Update deliviry_routes.js

--- routes/deliviry/deliviry_routes.js
+++ routes/deliviry/deliviry_routes.js
@@ -7,7 +7,7 @@ const { verifyDeliviry } = require('../../utility/verify_role_deliviry');
 const { verifyAdmin } = require('../../utility/verify_role_admin');
 router.post('/register',body("email").isEmail().isLength({min:6,max:50}).withMessage("type valid email"),body("password").isString().isLength({min:8,max:30}).withMessage("type valid password"),body("phone").isMobilePhone().isLength({min:10,max:15}).withMessage("type valid phone number"),body("userName").isLength({min:6,max:30}).withMessage("type valid user name"),deliviryControoler.registerFunc);
 
-router.patch('/verifyAccount',body("verifyCode").isString().isLength({min:1,max:5}),verifyToken,verifyDeliviry,deliviryControoler.confirmAccountFunc);
+router.patch('/verifyAccount',body("verifyCode").isString().isLength({min:1,max:5}),verifyToken,deliviryControoler.confirmAccountFunc);
 router.get('/deliviryInfo',verifyToken,verifyDeliviry,deliviryControoler.getDeliviryInfo);
 router.patch('/resetPassword',body("resetPasswordCode").isString().isLength({min:1,max:5}),body('password').isString().isLength({min:8,max:30}),deliviryControoler.resetPasswordFunc);
 
