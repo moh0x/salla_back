fb0f6ebffb94782964d66eadf3cf1f1e44281b17
moh0x <141264101+moh0x@users.noreply.github.com>
2024-07-31 02:56:39 -0700

    Update vendor_routes.js

--- routes/vendor/vendor_routes.js
+++ routes/vendor/vendor_routes.js
@@ -6,7 +6,7 @@ const {body,validationResult } = require("express-validator");
 const { verifyVendor } = require('../../utility/verify_role_vendor');
 const { verifyAdmin } = require('../../utility/verify_role_admin');
 router.post('/register',body("email").isEmail().isLength({min:6,max:50}).withMessage("type valid email"),body("password").isString().isLength({min:8,max:30}).withMessage("type valid password"),body("phone").isMobilePhone().isLength({min:10,max:15}).withMessage("type valid phone number"),body("userName").isLength({min:6,max:30}).withMessage("type valid user name"),vendorControoler.registerFunc);
-router.patch('/verifyAccount',body("verifyCode").isString().isLength({min:1,max:5}),verifyToken,verifyVendor,vendorControoler.confirmAccountFunc);
+router.patch('/verifyAccount',body("verifyCode").isString().isLength({min:1,max:5}),verifyToken,vendorControoler.confirmAccountFunc);
 router.get('/vendorInfo',verifyToken,verifyVendor,vendorControoler.getVendorInfo);
 router.patch('/resetPassword',body("resetPasswordCode").isString().isLength({min:1,max:5}),body('password').isString().isLength({min:8,max:30}),vendorControoler.resetPasswordFunc);
 
